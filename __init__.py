df0561d5af0689c204fdd27ae4897826a102f940
이동준 <118297684+leesasang@users.noreply.github.com>
2026-04-06 16:40:32 +0900

    Delete __init__.py

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .models import Person, LabInput, ItemResult
-from .catalog import TEST_CATEGORIES, TEST_GROUPS, LAB_META, DISPLAY_NAMES_KO, UNITS_DEFAULT
-from .interpreters import interpret_selected_items
-from .report import make_report, create_pdf_bytes
